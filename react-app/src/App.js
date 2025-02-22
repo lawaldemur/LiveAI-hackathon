ae2670185a19d882ec8359b6a3aeb23221e3d990
lawaldemur <vnk3247@nyu.edu>
2025-02-22 10:29:03 -0500

    Add initial frontend

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState(null);
+
+    const artStyles = [
+        "Surrealism",
+        "Minimalism",
+        "Postmodernism",
+        "Expressionism",
+        "Futurism",
+        "Conceptual Art",
+    ];
+
+    const fetchImage = async (style) => {
+        try {
+            const response = await fetch(`/api/getImage?style=${style}`);
+            const data = await response.json();
+            setImage(data.imageUrl);
+        } catch (error) {
+            console.error("Error fetching image:", error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h3>Pick the Core Aesthetic</h3>
+            </header>
+
+            <div className="style-selector-wrapper">
+                <ul>
+                    {artStyles.map((style) => (
+                        <li key={style} onClick={() => fetchImage(style)}>
+                            {style}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="style-image-wrapper">
+                {image && <img src={image} alt="Art Style" />}
+            </div>
+        </div>
+    );
 }
 
 export default App;
